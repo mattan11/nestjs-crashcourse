3efeb477cad27e45262071261187a489c5e57ac0
Marek Witkowski <marek.witkowski@onet.com.pl>
2023-08-05 20:50:29 +0200

    add trpc to server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const trpc = app.get('TrpcRouter');
+  trpc.applyMiddleware(app)
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
