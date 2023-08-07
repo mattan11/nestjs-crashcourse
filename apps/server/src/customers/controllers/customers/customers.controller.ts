f6c08050a8503ba239c7e597f337cdfcba849a00
Marek Witkowski <marek.witkowski@onet.com.pl>
2023-08-07 19:44:09 +0200

    handle getCustomer controller

--- apps/server/src/customers/controllers/customers/customers.controller.ts
+++ apps/server/src/customers/controllers/customers/customers.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CustomersService } from '@server/customers/services/customers/customers.service';
+import { Request, Response } from 'express';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
 
-  @Get('')
-  getCustomer() {
-    return this.customersService.findCustomer();
+  @Get(':id')
+  getCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response, // using req, res params must response with res.send() method
+  ) {
+    // return this.customersService.findCustomer(req.params.id);
+    const customer = this.customersService.findCustomer(id);
+    if (customer) {
+      res.status(HttpStatus.OK).send(customer);
+    } else {
+      res.status(HttpStatus.NOT_FOUND).send({ msg: 'Customer not found!' });
+    }
+  }
+
+  @Get('/search/:id')
+  searchCustomerById(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customersService.findCustomer(id);
+    if (!customer)
+      throw new HttpException('Customer not found!', HttpStatus.NOT_FOUND);
+
+    return customer;
   }
 }
--- apps/server/src/customers/services/customers/customers.service.ts
+++ apps/server/src/customers/services/customers/customers.service.ts
@@ -2,11 +2,30 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
+  private users = [
+    {
       id: 1,
-      email: 'test@test.pl',
+      email: 'user1@test.pl',
       createdAt: new Date(),
-    };
+    },
+    {
+      id: 2,
+      email: 'user2@test.pl',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'user3@test.pl',
+      createdAt: new Date(),
+    },
+    {
+      id: 4,
+      email: 'user4@test.pl',
+      createdAt: new Date(),
+    },
+  ];
+
+  findCustomer(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
